1701bb47ed7949a066e1205806de53bc88da5480
Steve Akinyemi <appcypher@outlook.com>
2018-12-26 17:44:13 +0100

    Refactor utf8 validation

--- src/llvm_backend/parser/parser.rs
+++ src/llvm_backend/parser/parser.rs
@@ -44,63 +44,55 @@ fn validate_utf8(bytes: &[u8]) -> bool {
     let mut cursor = 0;
 
     // There must be at least a byte for a valid utf-8 string
-    if cursor == length {
+    if length == 0 {
         return false;
     }
 
+    //
     while cursor < length {
-        if bytes[cursor] < 0x80 { // 1 byte
+        //
+        if bytes[cursor] < 0x80 { // 1-byte encoding
             cursor += 1;
-        } else if // 2 bytes
+        } else if // 2-byte encoding
             // Check if there is at least 2 bytes needed by following conditions.
-            (cursor < length - 2) &&
-            (bytes[cursor] > 0xC1 && bytes[cursor] < 0xE0) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x7F && bytes[cursor + 1] < 0xC0) { // byte_2 valid range
+            cursor < length - 2 &&
+            (bytes[cursor] >= 0xC2 && bytes[cursor] =< 0xDF) && // byte_1
+            (bytes[cursor + 1] >= 0x80 && bytes[cursor + 1] <= 0xBF) { // byte_2
             cursor += 2;
-        } else if // 3 bytes
+        } else if // 3-byte encoding
             // Check if there is at least 3 bytes needed by following conditions.
-            (cursor < length - 3) &&
-            (bytes[cursor] == 0xE0) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x9F && bytes[cursor + 1] < 0xC0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_3 valid range
+            cursor < length - 3 &&
+            (
+                (bytes[cursor] == 0xE0) && // byte_1
+                (bytes[cursor + 1] >= 0xA0 && bytes[cursor + 1] =< 0xBF) && // byte_2
+                (bytes[cursor + 2] >= 0x80 && bytes[cursor + 3] =< 0xBF) // byte_3
+            ) ||
+            (
+                ((bytes[cursor] >= 0xE1 && bytes[cursor] <= 0xEC) || (bytes[cursor] >= 0xEE && bytes[cursor] <= 0xEF)) && // byte_1
+                (bytes[cursor + 1] >= 0x80 && bytes[cursor + 1] <= 0xBF) && // byte_2
+                (bytes[cursor + 2] >= 0x80 && bytes[cursor + 3] <= 0xBF) // byte_3
+            ) ||
+            (
+                (bytes[cursor] == 0xED) && // byte_1
+                (bytes[cursor + 1] >= 0x80 && bytes[cursor + 1] <= 0x9F) && // byte_2
+                (bytes[cursor + 2] >= 0x80 && bytes[cursor + 3] <= 0xBF) // byte_3
+            ) {
             cursor += 3;
-        } else if // 3 bytes
-            // Check if there is at least 3 bytes needed by following conditions.
-            (cursor < length - 3) &&
-            (bytes[cursor] > 0xE0 && bytes[cursor] < 0xED) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x7F && bytes[cursor + 1] < 0xC0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_3 valid range
-            cursor += 3;
-        } else if // 3 bytes
-            // Check if there is at least 3 bytes needed by following conditions.
-            (cursor < length - 3) &&
-            (bytes[cursor] == 0xED) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x7F && bytes[cursor + 1] < 0xA0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_3 valid range
-            cursor += 3;
-        } else if // 4 bytes
+        } else if // 4-byte encoding
             // Check if there is at least 4 bytes needed by following conditions.
-            (cursor < length - 4) &&
-            (bytes[cursor] == 0xF0) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x80 && bytes[cursor + 1] < 0xC0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 2] < 0xC0) && // byte_3 valid range
-            (bytes[cursor + 3] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_4 valid range
-            cursor += 4;
-        } else if // 4 bytes
-            // Check if there is at least 4 bytes needed by following conditions.
-            (cursor < length - 4) &&
-            (bytes[cursor] > 0xF0 && bytes[cursor] < 0xF4) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x7F && bytes[cursor + 1] < 0xC0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 2] < 0xC0) && // byte_3 valid range
-            (bytes[cursor + 3] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_4 valid range
-            cursor += 4;
-        } else if // 4 bytes
-            // Check if there is at least 4 bytes needed by following conditions.
-            (cursor < length - 4) &&
-            (bytes[cursor] == 0xF4) && // byte_1 valid range
-            (bytes[cursor + 1] > 0x7F && bytes[cursor + 1] < 0xC0) && // byte_2 valid range
-            (bytes[cursor + 2] > 0x7F && bytes[cursor + 2] < 0xC0) && // byte_3 valid range
-            (bytes[cursor + 3] > 0x7F && bytes[cursor + 3] < 0xC0) { // byte_4 valid range
+            cursor < length - 4 &&
+            (
+                (bytes[cursor] == 0xF0) && // byte_1
+                (bytes[cursor + 1] >= 0x80 && bytes[cursor + 1] =< 0xBF) && // byte_2
+                (bytes[cursor + 2] >= 0x7F && bytes[cursor + 2] =< 0xBF) && // byte_3
+                (bytes[cursor + 3] >= 0x7F && bytes[cursor + 3] =< 0xBF) // byte_4
+            ) ||
+            (
+                ((bytes[cursor] >= 0xF1 && bytes[cursor] =< 0xF3) || (bytes[cursor] == 0xF4)) && // byte_1
+                (bytes[cursor + 1] >= 0x80 && bytes[cursor + 1] =< 0xBF) && // byte_2
+                (bytes[cursor + 2] >= 0x80 && bytes[cursor + 2] =< 0xBF) && // byte_3
+                (bytes[cursor + 3] >= 0x80 && bytes[cursor + 3] =< 0xBF) // byte_4
+            ) {
             cursor += 4;
         } else {
             return false
@@ -184,7 +176,6 @@ pub enum Error {
     MalformedReturnTypeInFunctionType,
     ReturnTypeDoesNotMatchReturnCountInFunctionType,
 }
-
 
 // TODO: Implement
 pub fn error_to_string(error: (Error, usize)) {
